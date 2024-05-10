6cbb8676eaf0836254787101ab7e2a993c2bf3c7
YunusEmreOzturk104 <ozturkyunusemre511@gmail.com>
2024-05-10 14:02:57 +0300

    Shoot System Commited

--- Assets/character/ScriptsCharacter/GunClampingSystem.cs
+++ Assets/character/ScriptsCharacter/GunClampingSystem.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class GunClampingSystem : MonoBehaviour
 {
+    Camera camera;
+    [SerializeField]
+    LayerMask zombieLayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
         
+    }
+    void Shoot()
+    {
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, zombieLayer))
+        {
+            hit.collider.gameObject.GetComponent<ZombieController>().TakeDamage();
+        }
     }
 }
