007a66c80628ef5775ffaa54acefca309a30a4ca
YunusEmreOzturk104 <ozturkyunusemre511@gmail.com>
2024-05-10 15:03:30 +0300

    zombie daage anim commited

--- Assets/Zombie 1/Scripts/ZombieController.cs
+++ Assets/Zombie 1/Scripts/ZombieController.cs
@@ -1,30 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieController : MonoBehaviour
 {
     [SerializeField]
     float zombieHP = 100;
-    Animator animator;
+    Animator zombieAnim;
+    GameObject targetPlayer;
+    [SerializeField]
+    float chasingDistance;
+    [SerializeField]
+    float attackDistance;
+    NavMeshAgent zombieNavMesh;
     // Start is called before the first frame update
     void Start()
     {
-        animator = this.gameObject.GetComponent<Animator>();
+        zombieAnim = this.gameObject.GetComponent<Animator>();
+        targetPlayer = GameObject.Find("Swat");
+        zombieNavMesh = this.gameObject.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         if (zombieHP <= 0)
         {
-            animator.SetBool("died", true);
+            zombieAnim.SetBool("died", true);
             StartCoroutine(DestroyZombie());
         }
         else
         {
-            //kodlanacak
+            float distance = Vector3.Distance(this.gameObject.transform.position, targetPlayer.transform.position);
+            if (distance < chasingDistance)
+            {
+                zombieNavMesh.isStopped = false;
+                zombieNavMesh.SetDestination(targetPlayer.transform.position);
+                zombieAnim.SetBool("walking", true);
+                zombieAnim.SetBool("punching", false);
+                this.transform.LookAt(targetPlayer.transform.position);
+            }
+            else
+            {
+                zombieNavMesh.isStopped = true;
+                zombieAnim.SetBool("walking", false);
+                zombieAnim.SetBool("punching", false);
+            }
+            if (distance < attackDistance)
+            {
+                this.transform.LookAt(targetPlayer.transform.position);
+                zombieNavMesh.isStopped = true;
+                zombieAnim.SetBool("walking", false);
+                zombieAnim.SetBool("punching", true);
+            }
         }
+    }
+    public void DoDamage()
+    {
+       // targetPlayer.GetComponent<CharacterController>().TakeDamage();
     }
     IEnumerator DestroyZombie()
     {
